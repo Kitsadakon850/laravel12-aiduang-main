5c4d9b436d9b68ae84bf1d189695bdfdf78561dd
Kitsadakon850 <kitsadakon850@gmail.com>
2025-09-25 10:41:24 +0700

    123

--- laravel12-aiduang-main/resources/views/teacher.blade.php
+++ laravel12-aiduang-main/resources/views/teacher.blade.php
@@ -2,7 +2,7 @@
     <x-theme title="Teacher">
         <div class="container">
             <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-                <h1 class="display-4">Teacherชอบหีครับ</h1>
+                <h1 class="display-4">Teacher</h1>
                 <p class="lead d-none">Quickly build an effective pricing table for your potential customers with this
                     Bootstrap
                     example. It’s built with default Bootstrap components and utilities with little customization.</p>
